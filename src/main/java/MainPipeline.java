1ac60d23a2c21410e754314d869d8534fdefdc67
21oavery <21oavery@ga.hiltoncsd.net>
2020-02-03 18:50:18 -0500

    Added approxPolyDP

--- src/main/java/MainPipeline.java
+++ src/main/java/MainPipeline.java
@@ -14,11 +14,24 @@ public class MainPipeline implements VisionPipeline {
         grip.process(mat);
     }
 
-    public void writeDebug(Mat matOut) {
-        Mat m = new Mat(240, 320, CV_8UC3, new Scalar(0, 0, 0));
+    public ArrayList<MatOfPoint> getContours() {
         ArrayList<MatOfPoint> conts = grip.filterContoursOutput();
         for (int i = 0; i < conts.size(); i++) {
-            drawRR(m, Imgproc.fitEllipse(make2f(conts.get(i))));
+            conts.set(i, approxPolyDP(conts.get(i)));
+        }
+        return conts;
+    }
+
+    public void writeDebug(Mat matOut) {
+        Mat m = new Mat(240, 320, CV_8UC3, new Scalar(0, 0, 0));
+        ArrayList<MatOfPoint> conts = getContours();
+        for (int i = 0; i < conts.size(); i++) {
+            try {
+                drawRR(m, Imgproc.fitEllipse(make2f(conts.get(i))));
+            } catch (Exception e) {
+                // Fit Ellipse can error if there are too few points
+                e.printStackTrace();
+            }
             Imgproc.drawContours(m, conts, 0, new Scalar(255, 255, 255), 1);
         }
         m.copyTo(matOut);
@@ -35,5 +48,17 @@ public class MainPipeline implements VisionPipeline {
 
     public static MatOfPoint2f make2f(MatOfPoint pIn) {
         return new MatOfPoint2f(pIn.toArray());
+    }
+
+    public static MatOfPoint makei(MatOfPoint2f pIn) {
+        return new MatOfPoint(pIn.toArray());
+    }
+
+    public static MatOfPoint approxPolyDP(MatOfPoint inS) {
+        MatOfPoint2f in = make2f(inS);
+        double e = 0.1 * Imgproc.arcLength(in, true);
+        MatOfPoint2f out = new MatOfPoint2f();
+        Imgproc.approxPolyDP(in, out, e, true);
+        return makei(out);
     }
 }
