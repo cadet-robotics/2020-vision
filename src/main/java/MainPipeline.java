e64f442d6f16208bb847687dd964fd79d4f50419
21oavery <21oavery@ga.hiltoncsd.net>
2020-02-03 18:26:38 -0500

    Bug Fix: Convert between points correctly

--- src/main/java/MainPipeline.java
+++ src/main/java/MainPipeline.java
@@ -18,7 +18,7 @@ public class MainPipeline implements VisionPipeline {
         Mat m = new Mat(240, 320, CV_8UC3, new Scalar(0, 0, 0));
         ArrayList<MatOfPoint> conts = grip.filterContoursOutput();
         for (int i = 0; i < conts.size(); i++) {
-            drawRR(m, Imgproc.fitEllipse(new MatOfPoint2f(conts.get(i))));
+            drawRR(m, Imgproc.fitEllipse(make2f(conts.get(i))));
             Imgproc.drawContours(m, conts, 0, new Scalar(255, 255, 255), 1);
         }
         m.copyTo(matOut);
@@ -31,5 +31,9 @@ public class MainPipeline implements VisionPipeline {
         for (int i = 0; i < 4; i++) {
             Imgproc.line(m, ps[i], ps[(i + 1) & 3], new Scalar(255, 0, 0), 2);
         }
+    }
+
+    public static MatOfPoint2f make2f(MatOfPoint pIn) {
+        return new MatOfPoint2f(pIn.toArray());
     }
 }
